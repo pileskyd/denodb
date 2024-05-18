89292398944571da3d5c78009fd62250c7f85f26
Denis Pilesky <mrdenysu@vk.com>
2024-05-19 03:18:33 +1000

    Fix MongoDb ver

--- deps.ts
+++ deps.ts
@@ -20,6 +20,6 @@ export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 export {
   Bson,
   MongoClient as MongoDBClient,
-} from "https://deno.land/x/mongo@v0.33.0/mod.ts";
-export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
-export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.33.0/src/database.ts";
+} from "https://deno.land/x/mongo@v0.28.1/mod.ts";
+export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
+export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.28.1/src/database.ts";
